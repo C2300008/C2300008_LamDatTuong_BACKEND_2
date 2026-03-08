549f2c00120e24a18c5ad9cc1bebc76be2d6512c
LamDatTuong <vkjb8kqm6x@privaterelay.appleid.com>
2026-03-08 23:12:11 +0700

    Cài đặt handle findone

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -36,8 +36,19 @@ exports.findAll = async (req, res, next) => {
   return res.send(documents);
 };
 
-exports.findOne = (req, res) => {
-  res.send({ message: "findOne handler" });
+exports.findOne = async (req, res, next) => {
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.find({ _id: req.params.id });
+    if (!document) {
+      return res.status(404).send({ message: "Contact not found" });
+    }
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, "Error retrieving contact with id=" + req.params.id),
+    );
+  }
 };
 
 exports.update = (req, res) => {
